be7e26f07034e67ba88e49e58eda8e633bea5190
zmath <theussalesm@gmail.com>
2024-11-04 23:12:56 -0300

    massa

--- api/generate.js
+++ api/generate.js
@@ -4,7 +4,6 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
-// Define a pasta de imagens de campo e jogadores
 const fieldImagesFolder = path.join(__dirname, 'images');
 const playersFolder = path.join(__dirname, 'players');
 
@@ -12,46 +11,39 @@ app.get('/api/generate', async (req, res) => {
   try {
     const { campo = 'normal', jogador1, jogador2 } = req.query;
 
-    // Define o caminho para a imagem do campo com base no tipo
     const fieldImagePath = (campo === 'normal') 
       ? path.join(fieldImagesFolder, 'campo.jpg') 
       : path.join(fieldImagesFolder, `${campo}.jpg`);
     
-    // Verifica se a imagem do campo existe
     if (!fs.existsSync(fieldImagePath)) {
       return res.status(404).send("Erro: Imagem do campo não encontrada.");
     }
 
-    // Carrega e redimensiona a imagem do campo
     const fieldImage = await sharp(fieldImagePath).resize(3463, 3464).toBuffer();
 
-    // Define os caminhos das imagens dos jogadores na pasta "players"
     const playerImages = [jogador1, jogador2]
       .map(jogador => path.join(playersFolder, `${jogador}.png`))
-      .filter(fs.existsSync); // Filtra para apenas arquivos que existam
+      .filter(filePath => fs.existsSync(filePath));
 
-    if (playerImages.length === 0) {
-      return res.status(400).send("Erro: Imagens dos jogadores não encontradas.");
+    if (playerImages.length < 2) {
+      return res.status(404).send("Erro: Uma ou mais imagens de jogadores não foram encontradas.");
     }
 
-    // Carrega e redimensiona as imagens dos jogadores
     const playerBuffers = await Promise.all(
       playerImages.map(async (imagePath) => {
         return await sharp(imagePath).resize(850, 950).toBuffer();
       })
     );
 
-    // Composição da imagem final
     let image = sharp(fieldImage);
     const layers = playerBuffers.map((buffer, index) => ({
       input: buffer,
       top: 200,
-      left: 100 + (index * 100), // Ajuste de espaçamento
+      left: 100 + (index * 100),
     }));
 
     image = image.composite(layers);
 
-    // Exporta a imagem final em formato webp
     const outputBuffer = await image.webp().toBuffer();
     res.setHeader('Content-Type', 'image/webp');
     res.send(outputBuffer);
