2c92edea8b4427e7830ae8847962ecb93e85efa5
zmath <theussalesm@gmail.com>
2024-11-23 22:09:05 -0300

    campos

--- api/generate.js
+++ api/generate.js
@@ -17,7 +17,7 @@ const formations = {
     ]
   },
   '4-3-3B': {
-    fieldImage: '433b.png',
+    fieldImage: '433B.png',
     positions: [
       { top: 791, left: 423 }, { top: 696, left: 223 }, { top: 699, left: 615 },
       { top: 572, left: 19 }, { top: 572, left: 822 }, { top: 479, left: 420 },
