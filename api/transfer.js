4c60da5a3ee44bb5e817d269441ce2dc3d69625e
zmath <theussalesm@gmail.com>
2024-12-20 22:17:37 -0300

    transferencias

---
+++ api/transfer.js
@@ -0,0 +1,60 @@
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
+const express = require('express')
+
+const app = express();
+
+const fieldImagesFolder = path.join(__dirname, 'images', 'transferencias.png');
+const playersFolder = path.join(__dirname, '..', 'players');
+
+app.get('/api/generate', async (req, res) => {
+  try {
+    const {
+      jogador1, jogador2, jogador3, dinheiro1, dinheiro2, dinheiro3
+    } = req.query;
+
+    const formation = formations[formacao] || formations['4-3-3'];
+
+    const fieldImagePath = path.join(fieldImagesFolder, formation.fieldImage);
+    if (!fs.existsSync(fieldImagePath)) {
+      return res.status(404).send(`Não foi possível encontrar a formação ${formation} no banco de imagens.`);
+    }
+
+    const fieldBuffer = await sharp(fieldImagePath).toBuffer();
+
+    const playerIds = [jogador1, jogador2, jogador3];
+
+    const processPlayerImage = async (id, index) => {
+      if (!id || id === 'nenhum') return null;
+
+      const imagePath = path.join(playersFolder, `${id}.png`);
+      if (!fs.existsSync(imagePath)) return null;
+
+      const buffer = await sharp(imagePath)
+        .resize(225, 250)
+        .toBuffer();
+      return { input: buffer, top: formation.positions[index].top, left: formation.positions[index].left };
+    };
+
+    const layers = await Promise.all(playerIds.map((id, index) => processPlayerImage(id, index)));
+    const validLayers = layers.filter(layer => layer);
+
+    const image = await sharp(fieldBuffer)
+      .composite(validLayers)
+      .webp({ quality: 90 })
+      .toBuffer();
+
+    res.setHeader('Content-Type', 'image/webp');
+    res.send(image);
+
+  } catch (error) {
+    console.error("Erro para gerar a imagem:", error.message);
+    res.status(500).send(`Erro para gerar a imagem: ${error.message}`);
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
